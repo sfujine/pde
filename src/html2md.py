0251553931c498810ee5b9ad94aebef4a9403201
km863 <45834251+km863@users.noreply.github.com>
2022-04-07 17:02:54 +0900

    Update html2md.py

--- src/html2md.py
+++ src/html2md.py
@@ -5,4 +5,5 @@ with request.urlopen('https://cloud.google.com/bigquery/docs/introduction?hl=ja'
     soup = bs(f.read())
 
 navi = soup.find('ul', class_='devsite-nav-list', menu='_book')
-
+for a in navi.find('a', class_='devsite-nav-title'):
+    print(a.text, a.get('href')) 
