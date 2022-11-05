ef045568e6b7137ce927b9850e55525918a2ca36
sfujine <45834251+sfujine@users.noreply.github.com>
2022-11-05 14:09:07 +0900

    Add files via upload

--- src/html2md.py
+++ src/html2md.py
@@ -5,25 +5,46 @@ from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup as bs
 
 
-target_url = sys.argv[1] 
+def extract(soup, pr):
+    contents = []
+    base = f'{pr.scheme}://{pr.netloc}'
 
-parse_results = urlparse(target_url)
-base = f'{parse_results.scheme}://{parse_results.netloc}'
-export_filename = f'{parse_results.path.split("/")[1]}.md'
+    navi = soup.find('ul', class_='devsite-nav-list', menu='_book')
+    for li in navi.findAll('li', recursive=False):
+        li_class = li['class']
+        if 'devsite-nav-preview' in li_class:
+            continue
+        if 'devsite-nav-heading' in li_class:
+            contents.append(f'## {li.text.strip()}')
+        for a in li.findAll('a', class_='devsite-nav-title'):
+            contents.append(f'### [{a.text.replace(" ", "")}]({urljoin(base, a.get("href"))})')
 
-with request.urlopen(target_url) as f:
-    soup = bs(f.read(), 'html.parser')
+    return contents
+    
+def get_soup(url):
+    with request.urlopen(url) as f:
+        soup = bs(f.read(), 'html.parser')
 
-contents = []
-navi = soup.find('ul', class_='devsite-nav-list', menu='_book')
-for li in navi.findAll('li', recursive=False):
-    li_class = li['class']
-    if 'devsite-nav-preview' in li_class:
-        continue
-    if 'devsite-nav-heading' in li_class:
-        contents.append(f'## {li.text.strip()}')
-    for a in li.findAll('a', class_='devsite-nav-title'):
-        contents.append(f'### [{a.text.replace(" ", "")}]({urljoin(base, a.get("href"))})')
+    return soup
+    
+def export(contents, pr):
+    pathes = pr.path.split("/")
+    service_name = pathes[1]
+    if service_name == 'ai-platform':
+        service_name = f'{service_name}-{pathes[2]}'
+    elif service_name == 'memorystore':
+        service_name = f'{service_name}-{pathes[3]}'
+        
+    with open(f'{service_name}.md', 'w', encoding='utf-8') as f:
+        f.writelines('\n'.join(contents))
 
-with open(export_filename, 'w', encoding='utf-8') as f:
-    f.writelines('\n'.join(contents))
+
+if __name__ == '__main__':
+    with open(sys.argv[1], encoding='utf-8') as f:
+        urls = f.readlines()
+        
+    for url in urls:
+        pr = urlparse(url)
+        soup = get_soup(url)
+        contents = extract(soup, pr)
+        export(contents, pr)
